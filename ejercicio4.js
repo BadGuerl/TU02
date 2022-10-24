b93c996659582d124f96dea3b60685008097da08
BadGuerl <mi_ailis@hotmail.com>
2022-10-24 17:08:16 +0200

    first commit

--- ejercicio4.js
+++ ejercicio4.js
@@ -3,3 +3,4 @@
 // pow(3, 3) = 3 * 3 * 3 = 27
 // pow(1, 100) = 1 * 1 * ...* 1 = 1
 // Para comprobar la función, muestra en la consola de JavaScript, a nivel de información, la salida de pow(2, 6), la salida de pow(0, 5) y la salida de(-5, 3)
+
