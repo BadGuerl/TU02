ea098c2e321784359d6ae9a80492a0a44914c55b
BadGuerl <mi_ailis@hotmail.com>
2022-10-24 17:28:56 +0200

    ejercicio 4

--- ejercicio4.js
+++ ejercicio4.js
@@ -4,3 +4,11 @@
 // pow(1, 100) = 1 * 1 * ...* 1 = 1
 // Para comprobar la función, muestra en la consola de JavaScript, a nivel de información, la salida de pow(2, 6), la salida de pow(0, 5) y la salida de(-5, 3)
 
+const potencia = Math.pow(2, 6);
+const potencia2 = Math.pow(0, 5);
+const potencia3 = Math.pow(-5, 3);
+
+
+console.log(potencia);
+console.log(potencia2);
+console.log(potencia3);
